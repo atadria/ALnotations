b1d60a7c55dd05bda6cfc2f65fc1bc3380d7267c
Adrianna Tokarska <adrianna.tokarska@comarch.com>
2021-08-08 19:16:34 +0200

    model update (training)

--- model/model.py
+++ model/model.py
@@ -1,30 +1,37 @@
 import spacy
 from spacy.training import Example
 
+from .language import load_model
+
 NER_PIPE = 'ner'
-MODELS_DICT = {
-    'pl': 'pl_core_news_lg',
-    'en': 'en_core_web_trf'
-}
 
 
 class NERModel(object):
     def __init__(self, language, disable_pipes=False):
-        assert language in MODELS_DICT
-        self.nlp = spacy.load(MODELS_DICT[language])
+        self.nlp = load_model(language)
+        self.language = language
         if disable_pipes:
             self.nlp.select_pipes(enable=NER_PIPE)
 
     @classmethod
     def load(cls, model_path):
         cls.nlp = spacy.load(model_path)
+        cls.language = cls.nlp.lang
 
-    def update(self, annotations, text):
+    def update(self, entities, text):
+        annotations = {'entities': entities}
         example = Example.from_dict(self.nlp.make_doc(text), annotations)
         self.nlp.update([example])
 
     def predict(self, text):
-        pass
+        doc = self.nlp(text)
+        entities = []
+        for ent in doc.ents:
+            entities.append([ent.start, ent.end, ent.label_])
+        return entities
 
     def to_disk(self, path):
         self.nlp.to_disk(path)
+
+    def get_ent_labels(self):
+        return self.nlp.get_pipe(NER_PIPE).labels
