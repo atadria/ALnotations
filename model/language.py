6e44055dae19f58b11c20c98048c52822b6ba4a5
Adrianna Tokarska <adrianna.tokarska@comarch.com>
2021-08-16 12:19:13 +0200

    change lg models to md, add new languages

--- model/language.py
+++ model/language.py
@@ -1,12 +1,19 @@
 import spacy
 
 MODELS_DICT = {
-    'pl': 'pl_core_news_lg',
-    'en': 'en_core_web_trf'
+    'pl': 'pl_core_news_md',
+    'en': 'en_core_web_md',
+    'de': 'de_core_news_md',
+    'fr': 'fr_core_news_md',
+    'es': 'es_core_news_md',
+    'it': 'it_core_news_md',
+    'el': 'el_core_news_md',
+    'xx': 'xx_ent_wiki_sm'
 }
 
 
 def load_model(language):
+    language = language.lower()
     if language in MODELS_DICT:
         return spacy.load(MODELS_DICT[language])
     else:
