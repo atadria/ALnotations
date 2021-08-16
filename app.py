afcf4683c7ce7957eebaa749b54a2cffea95e36e
Adrianna Tokarska <adrianna.tokarska@comarch.com>
2021-08-16 14:14:32 +0200

    base for flask app

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+from model.model import NERModel
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/process', methods=["POST"])
+def process():
+    if request.method == 'POST':
+        language = request.form['languageoption']
+        rawtext = request.form['rawtext']
+        nlp = NERModel(language)
+        results = nlp.predict(rawtext)
+        num_of_results = len(results)
+
+    return render_template("index.html", results=results, num_of_results=num_of_results)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
