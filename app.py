f246e8b9a0e35c5872886672417358c36b92b2d0
adri <tokarska.adrianna@gmail.com>
2021-08-17 19:08:42 +0200

    reading selected file

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/getfile', methods=['GET', 'POST'])
+def getfile():
+    if request.method == 'POST':
+        file = request.files['myfile']
+        text = file.readlines()
+        return render_template("index.html", results='', num_of_results='', text=text)
+
+
 @app.route('/process', methods=["POST"])
 def process():
     if request.method == 'POST':
@@ -19,7 +27,7 @@ def process():
         results = [(ent.label_, ent.text) for ent in nlp.predict(rawtext)]
         num_of_results = len(results)
 
-    return render_template("index.html", results=results, num_of_results=num_of_results)
+    return render_template("index.html", results=results, num_of_results=num_of_results, text='')
 
 
 if __name__ == '__main__':
